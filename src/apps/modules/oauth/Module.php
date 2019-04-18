c7891b8e7df5a2ab64609e82fb7b98f1df432189
natashaval <natasha.valentina1998@gmail.com>
2019-04-18 08:20:17 +0700

    module service

---
+++ src/apps/modules/oauth/Module.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Oauth;
+
+use Phalcon\DiInterface;
+use Phalcon\Loader;
+use Phalcon\Mvc\ModuleDefinitionInterface;
+
+class Module implements ModuleDefinitionInterface
+{
+    public function registerAutoloaders(DiInterface $di = null)
+    {
+        $loader = new Loader();
+
+        $loader->registerNamespaces([
+            'App\Oauth\Controllers' => __DIR__ . '/controllers',
+            'App\Oauth\Repositories' => __DIR__ . '/repositories',
+            'App\Oauth\Models' => __DIR__ . '/models'
+        ]);
+
+        $loader->register();
+    }
+
+    public function registerServices(DiInterface $di = null)
+    {
+        $moduleConfig = require __DIR__ . '/config/config.php';
+
+        $di->get('config')->merge($moduleConfig);
+
+        include_once __DIR__ . '/config/services.php';
+    }
+}
